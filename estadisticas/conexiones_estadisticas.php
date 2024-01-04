ede27f60921a7da43e1748b98493b066d74a1b6e
krakerbrain <marioplantabaja@gmail.com>
2024-01-04 20:41:31 -0300

    Se corrige error en funcion que no mostraba meses anteriores a enero del año actual

--- estadisticas/conexiones_estadisticas.php
+++ estadisticas/conexiones_estadisticas.php
@@ -112,9 +112,10 @@ function obtenerMesesEIngresos($con, $idusuario)
     for ($i = 5; $i >= 0; $i--) {
         $mes = ($mesActual - $i + 12) % 12; // Asegura que el mes esté en el rango de 1 a 12
         $mes = $mes == 0 ? 12 : $mes;
+        $annio = $mes > $mesActual ? $annioActual - 1 : $annioActual;
         $query = $con->prepare("SELECT extract(month FROM fecha) as mes, SUM(monto) as ingreso, count(destino) as conteo FROM viajes WHERE idusuario = :idusuario AND extract(year FROM fecha) = :anio AND extract(month FROM fecha) = :mes GROUP BY mes");
         $query->bindParam(':idusuario', $idusuario);
-        $query->bindParam(':anio', $annioActual);
+        $query->bindParam(':anio', $annio);
         $query->bindParam(':mes', $mes);
         $query->execute();
         $resultado = $query->fetch(PDO::FETCH_ASSOC);
