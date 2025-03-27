1e0b836d38a03e1872c2fe8ed56ad3f9cfb3afdc
Your Name <Mario Montenegro>
2025-03-26 23:07:53 -0300

    Se mejora el proceso de registro y recuperación de contraseña, actualizando la configuración del servidor SMTP y ajustando mensajes de éxito en el formulario de login.

--- mail/regMail.php
+++ mail/regMail.php
@@ -14,7 +14,7 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'SMTP.titan.email';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = $_ENV['DIRECCIONCORREO'];                     //SMTP username
     $mail->Password   = $_ENV['MAILPASS'];                              //SMTP password
@@ -35,10 +35,10 @@ try {
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Gracias por registrarte '.$usuario_registro;
+    $mail->Subject = 'Gracias por registrarte ' . $usuario_registro;
     $mail->Body    = '<div style="font-family: Arial, sans-serif; font-size: 16px; line-height: 1.5;">
 	                    <h1>¡Gracias por registrarte!</h1>
-	                    <p>Estimado/a '.$usuario_registro.',</p>
+	                    <p>Estimado/a ' . $usuario_registro . ',</p>
 	                    <p>¡Gracias por usar la app! La idea de esta aplicación es proporcionar una forma de organizar tus viajes</p>
 	                    <p>ya que la app de Boosmap por los momentos no lo hace.</p>
 	                    <p>Cualquier falla o mejora que consideres debería agregarse no dudes en comunicarme</p>
@@ -47,34 +47,33 @@ try {
                     </div>';
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-        //Server settings
-        $mailAviso->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-        $mailAviso->isSMTP();                                            //Send using SMTP
-        $mailAviso->Host       = 'SMTP.titan.email';                     //Set the SMTP server to send through
-        $mailAviso->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mailAviso->Username   = $_ENV['DIRECCIONCORREO'];                     //SMTP username
-        $mailAviso->Password   = $_ENV['MAILPASS'];                              //SMTP password
-        $mailAviso->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mailAviso->Port       = 465;  
+    //Server settings
+    $mailAviso->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mailAviso->isSMTP();                                            //Send using SMTP
+    $mailAviso->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mailAviso->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mailAviso->Username   = $_ENV['DIRECCIONCORREO'];                     //SMTP username
+    $mailAviso->Password   = $_ENV['MAILPASS'];                              //SMTP password
+    $mailAviso->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mailAviso->Port       = 465;
 
-        //Recipients
-        $mailAviso->setFrom($_ENV['DIRECCIONCORREO'], 'Registro de viajes Boosmap');
-        $mailAviso->addAddress($_ENV['CORREOADMIN'], 'Mario Montenegro');
-        $mailAviso->addReplyTo($_ENV['DIRECCIONCORREO'], 'Registro');
+    //Recipients
+    $mailAviso->setFrom($_ENV['DIRECCIONCORREO'], 'Registro de viajes Boosmap');
+    $mailAviso->addAddress($_ENV['CORREOADMIN'], 'Mario Montenegro');
+    $mailAviso->addReplyTo($_ENV['DIRECCIONCORREO'], 'Registro');
 
-            //Content
-        $mailAviso->isHTML(true); 
-        $mailAviso->Subject = 'Se ha registrado '.$usuario_registro;
-        $mailAviso->Body    = '<div style="font-family: Arial, sans-serif; font-size: 16px; line-height: 1.5;">
+    //Content
+    $mailAviso->isHTML(true);
+    $mailAviso->Subject = 'Se ha registrado ' . $usuario_registro;
+    $mailAviso->Body    = '<div style="font-family: Arial, sans-serif; font-size: 16px; line-height: 1.5;">
 	                    <h1>Se ha registrado un nuevo usuario</h1>
-	                    <p>'.$correo.',</p>
-	                    <p>'.$usuario_registro.'</p>
+	                    <p>' . $correo . ',</p>
+	                    <p>' . $usuario_registro . '</p>
                     </div>';
 
     $mail->send();
     $mailAviso->send();
     echo 'Message has been sent';
-    
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
